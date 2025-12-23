1ffbcf412dd3622e71cd3ff08f0f58b96b4fd137
Sofacherry <sofya.mokeeva.2005@mail.ru>
2025-12-23 05:00:37 +0300

    Update RelayCommand.cs

--- Center/RelayCommand.cs
+++ Center/RelayCommand.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Input;
 
 namespace EntertainmentCenter.Commands
@@ -22,5 +22,42 @@ namespace EntertainmentCenter.Commands
 
         public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
         public void Execute(object parameter) => _execute?.Invoke();
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter == null && typeof(T).IsValueType)
+                return false;
+            return _canExecute?.Invoke((T)parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is T typedParameter)
+            {
+                _execute?.Invoke(typedParameter);
+            }
+            else if (parameter == null && !typeof(T).IsValueType)
+            {
+                _execute?.Invoke(default);
+            }
+        }
     }
 }
